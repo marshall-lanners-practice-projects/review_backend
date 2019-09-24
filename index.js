60fcd95286af3862d2e150f5328d8cecabd425d4
lanners-marshall <lanners.marshall@gmail.com>
2019-09-24 00:42:59 -0700

    update

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ server.use(helmet());
 server.use(morgan('tiny'))
 server.use(cors())
 
+const userRoutes = require('./Routes/usersRoutes')
+
+server.use('/users', userRoutes)
+
 const port = process.env.PORT
 
 server.listen(port, () => console.log(`server running on port ${port}`));
