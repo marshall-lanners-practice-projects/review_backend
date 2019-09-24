57e6950e7c54944c4bba0e1ada8d9e7fe7b57770
lanners-marshall <lanners.marshall@gmail.com>
2019-09-24 01:19:39 -0700

    update

--- Routes/postsRoutes.js
+++ Routes/postsRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('knex')
+require('dotenv').config();
 const dbConfig = require('../knexfile')
 const db = knex(dbConfig.development)
 
