6dc792f94e895ffb4bb569793f66cad35e0322d9
lanners-marshall <lanners.marshall@gmail.com>
2019-09-24 00:37:31 -0700

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+const express = require('express');
+const server = express();
+const helmet = require('helmet')
+const cors = require('cors')
+const morgan = require('morgan')
+
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('tiny'))
+server.use(cors())
+
+const port = process.env.PORT
+
+server.listen(port, () => console.log(`server running on port ${port}`));
