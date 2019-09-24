e2583b28d7303faee124bac78f93c559f5d7249a
lanners-marshall <lanners.marshall@gmail.com>
2019-09-24 00:08:45 -0700

    update

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
-const DATABASE_URL = process.env.DB_USER
 
 module.exports = {
 
   production: {
     client: 'pg',
-    connection: DATABASE_URL + `?ssl=true`,
+    connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
       directory: './db/migrations'
     },
